3a0ae51e4b997ca93bdbf39b7e8e51c4436acfe1
gumm4 <luizduaetee@gmail.com>
2026-05-01 00:31:15 +0000

    No arquivo do JavaScript foram definidas as constantes que irão ser as responsáveis para a mudança de tema da página.
    Agora no arquivo CSS, foi apenas criado uma classe onde é definido as cores alternativas das páginas que serão mudadas.

--- Portifolio/js/script.js
+++ Portifolio/js/script.js
@@ -1,3 +1,13 @@
 const botao = document.getElementById("modoClaroEscuro");
+const body = document.body;
 
-console.log(botao)
+botao.addEventListener("click", () => {
+
+    body.classList.toggle("dark-mode");
+
+} );
+
+// Acima é criado as constantes do botão que irá mudar a cor tema da página, usando o id criado no arquivo HTML //
+// E temos a constante body, que pega diretamente da tag HTML, para fazer a transição de tema //
+
+//----------------------------------------------------------------------------------------------------//
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Portifolio
 Repositório para um portifolio pessoal, que apresentando meus conhecimentos e gostos
+
+Integrante: Luiz Gustavo Ferreira Duarte
